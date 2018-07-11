5c02db00f81090e7f3669c466d24c460755f4f9c
Xavi Torelló <info@xaviertorello.cat>
2018-07-11 17:08:40 +0200

    Client Spec skeleton

---
+++ specs/client_spec.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import)
+import vcr
+import io
+import csv
+import collections
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from lleida_net.click_sign import CS_API
+
+fixtures_path = 'specs/fixtures/client/'
+
+spec_VCR = vcr.VCR(
+    record_mode='new',
+    cassette_library_dir=fixtures_path
+)
+
+config = {
+    'user': 'user',
+    'password': 'key',
+    'environment': 'prod'
+}
+
+with description('A new CS client'):
+    with before.each:
+        with spec_VCR.use_cassette('init.yaml'):
+            self.config = config
+            self.expected = expected
+            self.client = CS_API(**config)
+
+    with context('initialization'):
+        with it('must be performed as expected'):
+            with spec_VCR.use_cassette('init.yaml'):
+                assert self.client
