24cc99b242eb3d361a2dc17d42e404f577cfaebb
Xavi Torelló <info@xaviertorello.cat>
2018-07-11 20:14:35 +0200

    Add test for login error detections

--- specs/api_spec.py
+++ specs/api_spec.py
@@ -46,15 +46,26 @@ with description('A new CS API'):
                 assert type(self.api.environment) == str, "Environment format is not the expected (str)"
                 assert self.api.environment == self.config['environment'], "Environment is not the expected one"
 
+        with it('must be performed as expected for errors'):
+            with spec_VCR.use_cassette('init.yaml'):
+                config = {
+                    'user': 'unnamed',
+                    'password': 'unnamed',
+                }
+                self.api = CS_API(**config)
+                response = self.api.post("get_config_list")
+                assert response.error, "It must return an error for incorrect logins"
+
 
     with context('config fetch'):
         with it('must work as expected'):
             with spec_VCR.use_cassette('get_config_list.yaml'):
                 response = self.api.post("get_config_list")
-
                 validate = serializers.ResponseSchema().load(response.result)
+
                 assert not validate.errors, "There must be no errors while validating API response"
 
+                assert not response.error, "Response must not contain errors"
                 assert response.code == 200, "Response code must be 200"
                 assert "result" in response, "A 'result' must be inside the response"
                 result = Munch(response.result)
