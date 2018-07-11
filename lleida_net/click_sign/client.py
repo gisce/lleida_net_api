927c610ed55cbd98b68915c8798f00c2417ed6af
Xavi Torelló <info@xaviertorello.cat>
2018-07-11 17:09:25 +0200

    Base CS_API Client
    Initialize a CS_API instance reaching credentials from the ENV if not 
    passed at init

---
+++ lleida_net/click_sign/client.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from .api import CS_API
+import os
+
+
+class Client(object):
+    def __init__(self, user=None, password=None, environment=None):
+
+        # Handle the user
+        self.user = user
+        if not user:
+            self.user = os.getenv('CS_user')
+        assert self.user, "The user is needed to initialize the Lleida.net Click'n'Sign connection"
+
+        # Handle the password
+        self.password = password
+        if not password:
+            self.password = os.getenv('CS_password')
+        assert self.password, "The password is needed to initialize the Lleida.net Click'n'Sign connection"
+
+        # Handle the env, by default prod
+        self.environment = "prod"
+        if environment:
+            self.environment = environment
+
+        self.API = CS_API(user=self.user, password=self.password, environment=self.environment)
