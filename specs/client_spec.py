a94998b8e31641a15a8d4b22eecfccffc8c26d5f
Xavi Torelló <info@xaviertorello.cat>
2018-07-11 18:50:40 +0200

    Test incorrect signature request

--- specs/client_spec.py
+++ specs/client_spec.py
@@ -63,3 +63,22 @@ with description('A new CS client'):
 
                     response = self.client.signature.start(data)
 
+            with it('must handle incorrect signature definitions'):
+                with spec_VCR.use_cassette('signature_start.yaml'):
+
+                    def incorrect_signature_start ():
+                        data = {
+                            "config_id": 12345,
+                            "contract_id": "ContractID",
+                            "file": [
+                                {
+                                    "filename": "contract.pdf",
+                                    "content": "{{base64_file_content}}",
+                                    "file_group": "contract_files"
+                                }
+                            ]
+                        }
+
+                        response = self.client.signature.start(data)
+
+                    expect(incorrect_signature_start).to(raise_error(NotValidSignatureSchemaException))
