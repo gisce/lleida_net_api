939c458922f5f766258717a577e636d25e13fcee
Xavi Torelló <info@xaviertorello.cat>
2018-07-11 18:50:27 +0200

    Test correct signature process

--- specs/client_spec.py
+++ specs/client_spec.py
@@ -25,10 +25,41 @@ with description('A new CS client'):
     with before.each:
         with spec_VCR.use_cassette('init.yaml'):
             self.config = config
-            self.expected = expected
-            self.client = CS_API(**config)
+            self.client = Client(**config)
 
     with context('initialization'):
         with it('must be performed as expected'):
             with spec_VCR.use_cassette('init.yaml'):
                 assert self.client
+
+    with context('Signature'):
+        with context('start'):
+            with it('must work as expected'):
+                with spec_VCR.use_cassette('signature_start.yaml'):
+                    data = {
+                        "config_id": 12345,
+                        "contract_id": "ContractID",
+                        "level": [
+                            {
+                                "level_order": 0,
+                                "signatories": [
+                                    {
+                                        "phone": "+34666666666",
+                                        "email": "signatory1@mail.com",
+                                        "name": "Name1",
+                                        "surname": "Surname1"
+                                    },
+                                ]
+                            },
+                        ],
+                        "file": [
+                            {
+                                "filename": "contract.pdf",
+                                "content": "{{base64_file_content}}",
+                                "file_group": "contract_files"
+                            }
+                        ]
+                    }
+
+                    response = self.client.signature.start(data)
+
