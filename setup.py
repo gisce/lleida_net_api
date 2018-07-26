42f054588244302bad19431331b34ecaca9d8685
Xavi Torelló <info@xaviertorello.cat>
2018-07-26 12:33:55 +0200

    Improve setup.py with reqs processing and MD long desc based on README

--- setup.py
+++ setup.py
@@ -6,17 +6,33 @@ from setuptools import setup, find_packages
 with io.open('lleida_net/__init__.py', 'rt', encoding='utf8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
-INSTALL_REQUIRES = ['Munch', 'Marshmallow']
+with open(f'requirements.txt', 'r') as f:
+    INSTALL_REQUIRES = f.readlines()
+
+try:
+    with open(f'requirements-dev.txt', 'r') as f:
+        TESTS_REQUIRE = f.readlines()
+except:
+        TESTS_REQUIRE = None
+
+try:
+    with open("README.md", "r") as readmefile:
+        long_description = readmefile.read()
+except:
+        long_description = None
 
 setup(
     name='lleida_net',
     description='Python client desired to interact with the Click&Sign API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=version,
     url='https://www.gisce.net',
     author='Xavi Torelló',
     author_email='xtorello@gisce.net',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
     license='General Public Licence 3',
     provides=['lleida_net'],
     classifiers = [
