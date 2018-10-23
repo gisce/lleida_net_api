894778594e2b8842e255ccd3270aedabf21b2ded
Xavi Torelló <info@xaviertorello.cat>
2018-10-23 12:15:10 +0200

    Add credentials to test config

--- specs/config.py
+++ specs/config.py
@@ -1,6 +1,11 @@
 from munch import Munch
 
 config = Munch({
+    'credentials': {
+        'user': 'user',
+        'password': 'key',
+        'environment': 'prod'
+    },
     'config_id': 984,
     'signatory_id': 15402849840400000,
 })
