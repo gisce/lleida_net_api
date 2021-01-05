7cbb16733f776e923939c216130b08fcaf7be7c5
Eduard Carreras <ecarreras@gisce.net>
2021-01-05 08:15:53 +0100

    Bump to v0.6.1

--- lleida_net/__init__.py
+++ lleida_net/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import (division, absolute_import, print_function, unicode_literals)
 
 __author__ = 'GISCE-TI, S.L.'
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 from . import click_sign
 from . import sms
