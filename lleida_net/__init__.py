08633c275aab701a4545e651e6dc83d33726c753
eberloso <eberloso@gisce.net>
2023-08-29 12:04:42 +0200

    Bump to v0.6.2

--- lleida_net/__init__.py
+++ lleida_net/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import (division, absolute_import, print_function, unicode_literals)
 
 __author__ = 'GISCE-TI, S.L.'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 from . import click_sign
 from . import sms
